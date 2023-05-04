2f69f53cebb58c0ea2fc3e260a00073a3bf6c8a8
Nix <tannernix447@gmail.com>
2023-05-04 00:02:48 -0600

    big change

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,60 @@
-// Assignment code here
+// Finished Assignment code here
 
-
-// Get references to the #generate element
+// All my global variables for my later functions
 var generateBtn = document.querySelector("#generate");
+var passwordLength = 8;
+var choiceOption = [];
+var specialCharacters = ['!','@','#','$','%','^','&','*','(',')','[',']'];
+var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var number = ['0','1','2','3','4','5','6','7','8','9'];
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+// Added a console.log to help me early on
+function generatePassword() {
+  console.log("Test click")
+  var password = "";
+  for(var i=0; i < passwordLength; i++) {
+    var randomCharacter = Math.floor(Math.random() * choiceOption.length);
+    password = password + choiceOption[randomCharacter];
+  }
+  return password;
 }
 
-// Add event listener to generate button
+// Function to set all my prompts with right criteria
+function openPrompts() {
+  choiceOption = [];
+  passwordLength = parseInt(prompt("How many characters do you want your password to be? (8 - 128)"));
+
+  if(isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    alert("Password length needs to be between 8 - 128 digits. Please try again.");
+    return false;
+  }
+  if (confirm("Would you like to include lowercase letters?")) {
+    choiceOption = choiceOption.concat(lowerCase);
+  }
+  if (confirm("Would you like to include uppercase letters?")) {
+    choiceOption = choiceOption.concat(upperCase);
+  }
+  if (confirm("Would you like to include special characters?")) {
+    choiceOption = choiceOption.concat(specialCharacters);
+  }
+  if (confirm("Would you like to include numbers?")) {
+    choiceOption = choiceOption.concat(number);
+  }
+  return true;
+}
+
+// Function for the final write password at the end
+function writePassword() {
+  var finalPrompts = openPrompts(); 
+
+  if(finalPrompts) {
+    var finalPassword = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = finalPassword;
+  } 
+}
+
+// Execuding all the js here
 generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Password Generator Starter Code
+# Name of my project
+
+friendly-parakeet-challenge-3
+
+## Description
+
+For this third homework assignment I was to create a password generator by letting the user prompt a few objectives and meet the certain criteria I was giving them all so they could produce a random generated password. This website could now help companies or individuals looking to get a secure and secret good password for any webpage or account that they needed a password for. Overall, I learned more about the basics of javascript and how to apply functions and variables better. Thank you!
+
+## Usage
+
+Link to website:
+
+
+## Credits
+
+Looked up how to apply certain details on google, used sources from MDN webdocks and our gitlab notes to know how to apply and write the code in the correct way. Also asked TA for help on not forking the code. Lastly, most of the starter code was given to me from this webpages github.
+
+Main, MDN webpage: 
+https://developer.mozilla.org/en-US/
+
+Starter code:
+https://github.com/coding-boot-camp/friendly-parakeet
+
